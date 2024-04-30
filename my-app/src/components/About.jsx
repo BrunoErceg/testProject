d20628c46be35620a4ca60e67940f7d1859f5563
BrunoErceg <brunoerceg95@gmail.com>
2024-04-30 11:28:43 +0200

    About Section Added

--- my-app/src/components/About.jsx
+++ my-app/src/components/About.jsx
@@ -1,13 +1,31 @@
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3Alt } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaReact } from "react-icons/fa6";
+
 export default function About () {
     return(
         <section className="about bg-black text-color-white ">
             <div className="container">
-                <h2>A Littel About Me</h2>
-                <p>
-                    Quisque consectetur vel sapien id malesuada. Duis sit amet turpis quis arcu consectetur malesuada. 
-                    Nunc at sodales odio. Aliquam et arcu maximus, rhoncus ipsum non, bibendum ante. 
-                    Aliquam euismod diam sit amet eleifend efficitur.
-                </p>
+                <div className="row">
+                    <div className="col">
+                        <FaHtml5 className="icon text-color-green" />
+                        <p>HTML5</p>
+                    </div>
+                    <div className="col">
+                        <FaCss3Alt className="icon text-color-green"/>
+                        <p>CSS3</p>
+                    </div>
+                    <div className="col">
+                        <IoLogoJavascript  className="icon text-color-green"/>
+                        <p>JAVASCRIPT</p>
+                    </div>
+                    <div className="col">
+                        <FaReact className="icon text-color-green" />  
+                        <p>REACT</p>
+                    </div>
+                </div>
+                
             </div>
         </section>
     )
